c5337fbbd0aa9a30c231e64714687e45e09d0d84
Kumar Ramamoorthy, Senthil <senthil.kumar-ramamoorthy@capgemini.com>
2023-05-21 01:19:45 +0530

    ps2

--- mypsscript.ps1
+++ mypsscript.ps1
@@ -1,4 +1,4 @@
-Write-Host "Hello, World Webhook trigger with /"
+Write-Host "Hello, World Webhook trigger"
 $file = Get-Date -UFormat %H%M%S
 touch $file
 Write-Host "File $file Created sucessfully now"
