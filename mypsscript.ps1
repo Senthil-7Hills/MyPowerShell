408b5a6ddb4f672602010482e970eb6452657d7d
Kumar Ramamoorthy, Senthil <senthil.kumar-ramamoorthy@capgemini.com>
2023-05-23 12:43:12 +0530

    vscode

--- mypsscript.ps1
+++ mypsscript.ps1
@@ -1,7 +1,8 @@
-Write-Host "Hello, World Webhook trigger 2105"
+Write-Host "Hello, World Webhook trigger from VScode"
 $file = Get-Date -UFormat %H%M%S
 touch $file
 Write-Host "File $file Created sucessfully on" (Get-Date)
 Write-Host "+++++++++++++"
+safds
 
 
