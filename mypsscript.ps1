f8bef15715ef6090abd2fca4238e73b700bc39d3
Senthil Kumar R <60551158+Senthil-7Hills@users.noreply.github.com>
2023-05-23 12:36:52 +0530

    Update mypsscript.ps1

--- mypsscript.ps1
+++ mypsscript.ps1
@@ -1,4 +1,4 @@
-Write-Host "Hello, World Webhook trigger for my team"
+Write-Host "Hello, World Webhook trigger from git"
 $file = Get-Date -UFormat %H%M%S
 touch $file
 Write-Host "File $file Created sucessfully on" (Get-Date)
